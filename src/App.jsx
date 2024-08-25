b1db127d4e94c5c0edbdcbd3c7272b9e058c4104
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-08-26 01:49:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-600 text-center">Shah Arefin Ahmed</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
