63a64967bcfcf46ea6fa0018c1064c4493b8aea0
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-08-27 21:32:48 +0600

    Location section has been done

--- src/pages/Home/Location/Location.jsx
+++ src/pages/Home/Location/Location.jsx
@@ -1,44 +1,42 @@
 import img3 from "../../../assets/Location/Frame.png"
+import img1 from "../../../assets/Location/Frame1.png"
+import img2 from "../../../assets/Location/Frame2.png"
 
 const Location = () => {
   return (
-    <div className="grid lg:grid-cols-3 grid-cols-1 my-5">
+    <div className="grid lg:grid-cols-3 grid-cols-1 my-5 ml-10">
       {/* This is first card */}
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="card bg-[#07332F] text-white w-96 h-[202px] shadow-xl">
+        <div className="card-body flex lg:flex-row flex-col">
+          <img className="w-[50px] h-[50px] ml-5 mr-5" src={img2} alt="" />
+          <div>
+            <h2 className="text-[25px] font-bold">Opening Hours</h2>
+            <div className="flex flex-col">
+              <h2 className="text-[20px] font-normal">
+                Open 9.00 am to 5.00pm
+              </h2>
+              <h2 className="text-[20px] font-normal">Everyday</h2>
+            </div>
           </div>
         </div>
       </div>
       {/* This is second card */}
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div className="card bg-[#F7A582] text-white w-96 h-[202px] shadow-xl">
+        <div className="card-body flex lg:flex-row flex-col">
+          <img className="w-[50px] h-[50px] ml-5 mr-5" src={img1} alt="" />
+          <div>
+            <h2 className="text-[25px] font-bold">Our Locations</h2>
+            <div className="flex flex-col">
+              <h2 className="text-[20px] font-normal">Dhanmondi 17, Dhaka</h2>
+              <h2 className="text-[20px] font-normal">-1200, Bangladesh</h2>
+            </div>
           </div>
         </div>
       </div>
       {/* This is third card */}
       <div className="card bg-[#07332F] text-white w-96 h-[202px] shadow-xl">
         <div className="card-body flex lg:flex-row flex-col">
-          <img className="w-[50px] h-[50px] ml-12 mr-5" src={img3} alt="" />
+          <img className="w-[50px] h-[50px] ml-5 mr-5" src={img3} alt="" />
           <div>
             <h2 className="text-[25px] font-bold">Contact Us</h2>
             <div className="flex flex-col">
